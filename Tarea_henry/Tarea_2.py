835c5ad1501613302b457ca0157c016ebe4a5118
Julián Eduardo Mena Chávez <julinkchido@gmail.com>
2022-02-12 14:33:59 -0600

    Tarea terminada

--- Tarea_henry/Tarea_2.py
+++ Tarea_henry/Tarea_2.py
@@ -1,5 +1,3 @@
-from sqlite3 import enable_callback_tracebacks
-
 
 # 1.- Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla si es mayor o menor a cero
 number=2
@@ -75,17 +73,147 @@ for i in range(1, n):
 # 9.- Imprimir los números primos existentes entre 0 y 30
 
 
-
+tope_rango=30
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
 
 # 10.- ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
+
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+
+
 # 11.- En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
+
+
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
+
+
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
 
 # 12.- Si la cantidad de números que se evalúa es mayor a treinta, esa optimización crece?
 
+tope_rango=100
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
+
+
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
+
 # 13.- Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
+
+
+n = 99
+while(n <= 300):
+    n += 1
+    if (n % 12 != 0):
+        continue
+    print(n, ' es divisible por 12')
+
 
 # 14.-  Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
+
+n = 1
+sigue = 1
+primo = True
+while (sigue == 1):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+            break
+    if (primo):
+        print(n)
+        print('¿Desea encontrar el siguiente número primo?')
+        if (input() != '1'):
+            print('Se finaliza el proceso')
+            break
+    else:
+        primo = True
+    n += 1
+
+
 # 15.-  Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
+
+n = 100
+while(n<=300):
+    if (n % 6 == 0):
+        print('El número es: ', str(n))
+        break
+    n += 1
